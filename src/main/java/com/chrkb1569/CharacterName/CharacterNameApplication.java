4e0136805749366181f76639471bcd5b3bcfe5f8
minsub <chrkb1569@naver.com>
2024-02-18 17:02:41 +0900

    feat : Init Project

---
+++ src/main/java/com/chrkb1569/CharacterName/CharacterNameApplication.java
@@ -0,0 +1,13 @@
+package com.chrkb1569.CharacterName;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chrkb1569/CharacterName/CharacterNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrkb1569.CharacterName;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
