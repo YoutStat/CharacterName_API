a48915a90c9c08221816b97b2476a6872c8d41bf
minsub <chrkb1569@naver.com>
2024-02-19 19:58:19 +0900

    feat : 예외 처리 열거형 클래스 및 예외 파일 추가

---
+++ src/main/java/com/chrkb1569/CharacterName/exception/APIResultException.java
@@ -0,0 +1,7 @@
+package com.chrkb1569.CharacterName.exception;
+
+public class APIResultException extends RuntimeException {
+    public APIResultException(String message) {
+        super(message);
+    }
+}
