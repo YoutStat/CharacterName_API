8613b633a18d3445a97700ea3974ff87fcddffcf
minsub <chrkb1569@naver.com>
2024-02-23 23:37:33 +0900

    feat : APIService 로직 추가 - 캐릭터 식별자 호출

--- src/main/java/com/chrkb1569/CharacterName/service/APIService.java
+++ src/main/java/com/chrkb1569/CharacterName/service/APIService.java
@@ -8,14 +8,17 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.time.LocalDate;
 
 @Service
 public class APIService {
     private final String API_KEY; // API를 호출하기 위한 Key값
-    private final String HTTP_REQUEST_URL; // API를 요청하기 위한 URL
+    private final String HTTP_REQUEST_NAME_URL; // API를 요청하기 위한 URL
+    private final String HTTP_REQUEST_IDENTIFIER_URL; // API를 요청하기 위한 URL
     private final String HTTP_REQUEST_DATE; // API 요청시에 사용되는 날짜
     private final String HTTP_REQUEST_METHOD; // API 요청시에 사용되는 HTTP Method
     private final String HTTP_REQUEST_HEADER; // API 요청시에 사용되는 HTTP 헤더값
@@ -23,18 +26,20 @@ public class APIService {
 
     public APIService(
             @Value("${api.request.key}") String key,
-            @Value("${api.request.url}") String requestUrl,
+            @Value("${api.request.name.url}") String requestNameUrl,
+            @Value("${api.request.identifier.url}") String requestIdentifierUrl,
             @Value("${api.info.requestMethod}") String requestMethod,
             @Value("${api.info.requestHeader}") String requestHeader
     ) {
         this.API_KEY = key;
-        this.HTTP_REQUEST_URL = requestUrl;
+        this.HTTP_REQUEST_NAME_URL = requestNameUrl;
+        this.HTTP_REQUEST_IDENTIFIER_URL = requestIdentifierUrl;
         this.HTTP_REQUEST_DATE = LocalDate.now().minusDays(DATE_GAP).toString();
         this.HTTP_REQUEST_METHOD = requestMethod;
         this.HTTP_REQUEST_HEADER = requestHeader;
     }
 
-    public String getAPIData(final int pageNumber) {
+    public String getCharacterNames(final int pageNumber) {
         try {
             HttpURLConnection connection = getURLConnection(pageNumber);
 
@@ -44,19 +49,48 @@ public class APIService {
         }
     }
 
+    public String getCharacterIdentifier(final String characterName) {
+        try {
+            String encodedName = encodeCharacterName(characterName);
+            HttpURLConnection connection = getURLConnection(encodedName);
+
+            return getAPIResult(connection);
+        } catch (IOException e) {
+            throw new APIRequestException();
+        }
+    }
+
+    private String encodeCharacterName(String characterName) throws UnsupportedEncodingException {
+        return URLEncoder.encode(characterName, "UTF-8");
+    }
+
     private HttpURLConnection getURLConnection(final int pageNumber) throws IOException {
         URL url = getURL(pageNumber);
 
-        // HTTP connection 설정
+        return getUrlConnection(url);
+    }
+
+    private HttpURLConnection getURLConnection(final String characterName) throws IOException {
+        URL url = getURL(characterName);
+
+        return getUrlConnection(url);
+    }
+
+    private URL getURL(final int pageNumber) throws IOException {
+        return new URL(String.format(HTTP_REQUEST_NAME_URL, HTTP_REQUEST_DATE, pageNumber));
+    }
+
+    private URL getURL(final String characterName) throws IOException {
+        return new URL(String.format(HTTP_REQUEST_IDENTIFIER_URL, characterName));
+    }
+
+    // HTTP connection 설정
+    private HttpURLConnection getUrlConnection(URL url) throws IOException {
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
         connection.setRequestMethod(HTTP_REQUEST_METHOD);
         connection.setRequestProperty(HTTP_REQUEST_HEADER, API_KEY);
 
         return connection;
-    }
-
-    private URL getURL(final int pageNumber) throws IOException {
-        return new URL(String.format(HTTP_REQUEST_URL, HTTP_REQUEST_DATE, pageNumber));
     }
 
     private String getAPIResult(HttpURLConnection connection) throws IOException {
