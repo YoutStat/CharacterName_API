84ef06df0e5cb3436f92e3ed3b22a8fbae0ec295
minsub <chrkb1569@naver.com>
2024-03-18 02:19:01 +0900

    style : 패키지 변경 config -> config.s3

--- src/main/java/com/chrkb1569/CharacterName/config/s3/S3Config.java
+++ src/main/java/com/chrkb1569/CharacterName/config/s3/S3Config.java
@@ -1,4 +1,4 @@
-package com.chrkb1569.CharacterName.config;
+package com.chrkb1569.CharacterName.config.s3;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
